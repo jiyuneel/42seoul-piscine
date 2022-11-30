49c95c8a2b508db5578ece3c5d220ab810809516
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 18:19:19 +0900

    ex04

---
+++ c08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 15:50:02 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/09/08 17:54:29 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	struct s_stock_str	*arr;
+	int					i;
+
+	arr = (struct s_stock_str *)malloc(sizeof(struct s_stock_str) * (ac + 1));
+	if (arr == 0)
+		return (0);
+	i = 0;
+	while (i < ac)
+	{
+		arr[i].size = ft_strlen(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = (char *)malloc(sizeof(char) * arr[i].size + 1);
+		ft_strcpy(arr[i].copy, arr[i].str);
+		i++;
+	}
+	arr[i].str = 0;
+	return (arr);
+}
