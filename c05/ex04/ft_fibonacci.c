a02d6b51108c40caf0ba339ba9ab7a8d646a6cca
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 18:02:09 +0900

    ex04

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 12:51:56 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/09/03 12:51:58 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
