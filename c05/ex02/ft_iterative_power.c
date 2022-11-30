9784af71f49abbcd4bda0f0f9f5b435b345db6cf
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 18:02:04 +0900

    ex02

---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 22:27:01 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/09/04 11:57:44 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	i;
+	int	result;
+
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	i = 0;
+	result = 1;
+	while (i < power)
+	{
+		result *= nb;
+		i++;
+	}
+	return (result);
+}
