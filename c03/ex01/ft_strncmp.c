bf106829cb58c9bb2bd5b3dfb5597b5c3ba02b8e
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 17:53:49 +0900

    ex01

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 12:35:57 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/08/29 12:35:58 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	if (i < n)
+	{
+		if (s1[i])
+			return (1);
+		else if (s2[i])
+			return (-1);
+	}
+	return (0);
+}
