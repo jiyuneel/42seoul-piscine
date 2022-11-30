ab81742d02cc993f7b660e28734f048e90a9b7e9
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 18:04:55 +0900

    ex03

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 17:52:37 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/09/01 17:52:39 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_str(char **argv, int size)
+{
+	char	*tmp;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i <= size)
+	{
+		j = i + 1;
+		while (j <= size)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	ft_sort_str(argv, argc - 1);
+	i = 1;
+	while (i < argc)
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n", 1);
+		i++;
+	}
+}
