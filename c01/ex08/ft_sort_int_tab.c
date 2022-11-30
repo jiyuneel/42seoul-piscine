da7f4cd61239e3b9fb05ff95df05b47e25c18fe7
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 17:47:25 +0900

    ex08

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 12:56:08 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/08/27 12:57:14 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	tmp;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
