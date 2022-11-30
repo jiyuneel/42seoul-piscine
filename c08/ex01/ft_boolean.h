8b7eeda1f3d5435567924006ec12ac61f9e0b6aa
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 18:18:43 +0900

    ex01

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 14:42:35 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/09/08 17:53:44 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define EVEN(nbr) (nbr % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
