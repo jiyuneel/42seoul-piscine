d4aa9b05a9626ecfff258e753a01286c5129d43f
Jiyun Lee <jiyunlee@c2r10s2.42seoul.kr>
2022-11-30 17:51:04 +0900

    ex08

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 22:09:53 by jiyunlee          #+#    #+#             */
+/*   Updated: 2022/08/28 22:10:05 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_uppercase(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (is_uppercase(str[i]))
+			str[i] = str[i] - 'A' + 'a';
+		i++;
+	}
+	return (str);
+}
